66991dad88ec6c53d585c5e7b87c81a0aa67152f
Shahid Latif <shahid.latif@seera.sa>
2022-02-22 10:44:31 +0400

    initital setup

---
+++ TestXcodeServer/TestXcodeServerUITests/TestXcodeServerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestXcodeServerUITestsLaunchTests.swift
+//  TestXcodeServerUITests
+//
+//  Created by Shahid Latif on 22/02/2022.
+//
+
+import XCTest
+
+class TestXcodeServerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
