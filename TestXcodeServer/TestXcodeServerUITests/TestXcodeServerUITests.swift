7d2e17549dba42edabd8547912cc8f361e8f617e
Shahid Latif <shahid.latif@seera.sa>
2022-02-24 12:04:03 +0400

    add build error2

--- TestXcodeServer/TestXcodeServerUITests/TestXcodeServerUITests.swift
+++ TestXcodeServer/TestXcodeServerUITests/TestXcodeServerUITests.swift
@@ -26,7 +26,7 @@ class TestXcodeServerUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-        XCTAssertTrue(false, "Intentionally Failed Test")
+        XCTAssertTrue(true, "Intentionally Failed Test")
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
